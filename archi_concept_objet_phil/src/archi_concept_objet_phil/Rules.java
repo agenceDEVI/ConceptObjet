be7059000a86994e71c67f1398a7c3dd37848b26
Unknown <benjamin.legrand@isen.yncrea.fr>
2019-11-04 21:51:35 +0100

    1 an après l'apocalypse, ALLLO !! IL RESTE DES SURVIVANTS ??

--- archi_concept_objet_phil/src/archi_concept_objet_phil/Rules.java
+++ archi_concept_objet_phil/src/archi_concept_objet_phil/Rules.java
@@ -6,5 +6,13 @@ public class Rules {
 	public static final int worldMap_maxX = 25;
 	public static final int worldMap_sizeSafeZone = 5;
 	
+	public static final int humain_PV_Max = 10;
+	public static final int elfe_PV_Max = 10;
+	public static final int orc_PV_Max = 10;
+	public static final int gobelin_PV_Max = 10;
 	
+	public static final int humain_PE_Max = 10;
+	public static final int elfe_PE_Max = 10;
+	public static final int orc_PE_Max = 10;
+	public static final int gobelin_PE_Max = 10;	
 }
